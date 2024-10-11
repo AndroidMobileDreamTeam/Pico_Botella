fae43b60b389ae75a702f7a34a64d2a3d80d9cb4
Samuel Galindo <itzsaky@outlook.com>
2024-10-11 00:17:40 -0500

    I added animations for bottle, button and text and some functionalities.

--- app/src/main/java/com/example/pico_botella/MainActivity.kt
+++ app/src/main/java/com/example/pico_botella/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.example.pico_botella
 
 import android.os.Bundle
+import android.view.View
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pico_botella.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
-    // Método para iniciar la animación del TextView
-    private fun startTextViewAnimation() {
-        val textv_presioname = binding.textvPresioname
-        val animation = AnimationUtils.loadAnimation(this, R.anim.grow)
-        textv_presioname.startAnimation(animation)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +18,51 @@ class MainActivity : AppCompatActivity() {
 
         // Iniciar la animación
         startTextViewAnimation()
+        buttonStartAnimation()
+        buttonStartClick()
+    }
+
+    private fun bottleAnimation() {
+        val bottle = binding.bottle
+        val animation = AnimationUtils.loadAnimation(this, R.anim.rotation)
+        bottle.startAnimation(animation)
+    }
+
+    private fun startTextViewAnimation() {
+        val textV = binding.textvPresioname
+        val animation = AnimationUtils.loadAnimation(this, R.anim.grow)
+        textV.startAnimation(animation)
+    }
+
+    private fun buttonStartAnimation() {
+        val button = binding.btnStart
+        val animation = AnimationUtils.loadAnimation(this, R.anim.grow)
+        button.startAnimation(animation)
+    }
+
+    private fun fadeTextvAnimation() {
+        val textV = binding.textvPresioname
+        val animation = AnimationUtils.loadAnimation(this, R.anim.fade)
+        textV.startAnimation(animation)
+    }
+
+    private fun fadeButtonAnimation() {
+        val button = binding.btnStart
+        val animation = AnimationUtils.loadAnimation(this, R.anim.fade)
+        button.startAnimation(animation)
+    }
+
+    private fun buttonStartClick() {
+        val button = binding.btnStart
+        val textV = binding.textvPresioname
+        button.setOnClickListener {
+            button.clearAnimation()
+            textV.clearAnimation()
+            fadeTextvAnimation()
+            textV.visibility = View.INVISIBLE
+            fadeButtonAnimation()
+            button.visibility = View.INVISIBLE
+            bottleAnimation()
+        }
     }
 }
