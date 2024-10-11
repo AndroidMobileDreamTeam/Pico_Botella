d003ba5d00a8260f9becfa2137d01bc0b86c3f40
Samuel Galindo <itzsaky@outlook.com>
2024-10-10 22:09:50 -0500

    I added the wooden background, the bottle and the "presioname" text

--- app/src/main/java/com/example/pico_botella/MainActivity.kt
+++ app/src/main/java/com/example/pico_botella/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.example.pico_botella
 
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pico_botella.databinding.ActivityMainBinding
-import androidx.databinding.DataBindingUtil.setContentView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    // Método para iniciar la animación del TextView
+    private fun startTextViewAnimation() {
+        val textv_presioname = binding.textvPresioname
+        val animation = AnimationUtils.loadAnimation(this, R.anim.grow)
+        textv_presioname.startAnimation(animation)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = setContentView(this, R.layout.activity_main)
-        }
+
+        // Inflar el layout usando View Binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Iniciar la animación
+        startTextViewAnimation()
     }
+}
