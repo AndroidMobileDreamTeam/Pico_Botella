7b12516c7b893734cec743a2c4a0512b3b329bb1
Samuel Galindo <itzsaky@outlook.com>
2024-11-26 18:21:44 -0500

    Implemented logic for login, register and navigation

--- app/src/main/java/com/example/spin_bottle/view/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/spin_bottle/view/fragment/LoginFragment.kt
@@ -11,7 +11,11 @@ import androidx.fragment.app.Fragment
 import com.example.spin_bottle_app.databinding.LoginFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.example.spin_bottle.model.AuthStatus
 import com.example.spin_bottle.viewmodel.AuthViewModel
+import com.example.spin_bottle_app.R
 
 @AndroidEntryPoint
 class LoginFragment: Fragment() {
@@ -25,8 +29,10 @@ class LoginFragment: Fragment() {
         binding = LoginFragmentBinding.inflate(layoutInflater)
 
         enableButtons()
-        loginUser()
         registerUser()
+
+        loginUser()
+
 
         return binding.root
     }
@@ -84,16 +90,6 @@ class LoginFragment: Fragment() {
         })
     }
 
-    private fun loginUser(){
-        val loginButton = binding.loginButton
-        loginButton.setOnClickListener {
-            val email = binding.emailInput.text.toString()
-            val password = binding.passwordInput.text.toString()
-
-            authViewModel.loginUser(email, password)
-        }
-    }
-
     private fun registerUser(){
         val registerButton = binding.registerText
         registerButton.setOnClickListener {
@@ -101,6 +97,44 @@ class LoginFragment: Fragment() {
             val password = binding.passwordInput.text.toString()
 
             authViewModel.registerUser(email, password)
+            registerStatus()
         }
+    }
+
+    private fun registerStatus(){
+        this.authViewModel.registerStatus.observe(viewLifecycleOwner, Observer { status ->
+            when (status) {
+                is AuthStatus.Success -> {
+                    findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+                }
+                is AuthStatus.Error -> {
+                    Toast.makeText(context, status.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+    }
+
+    private fun loginUser(){
+        val loginButton = binding.loginButton
+        loginButton.setOnClickListener {
+            val email = binding.emailInput.text.toString()
+            val password = binding.passwordInput.text.toString()
+
+            authViewModel.loginUser(email, password)
+            loginStatus()
+        }
+    }
+
+    private fun loginStatus(){
+        this.authViewModel.loginStatus.observe(viewLifecycleOwner, Observer { status ->
+            when (status) {
+                is AuthStatus.Success -> {
+                    findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+                }
+                is AuthStatus.Error -> {
+                    Toast.makeText(context, status.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
     }
 }
