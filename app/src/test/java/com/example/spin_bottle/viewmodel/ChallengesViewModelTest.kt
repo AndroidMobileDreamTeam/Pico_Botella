99b4036505b1e14fc99c1e05fdbbdd513c894e80
John <john.freddy.belalcazar@correounivalle.edu.co>
2024-11-27 23:35:20 -0500

    challenges y pokemons +50 coverage

--- app/src/test/java/com/example/spin_bottle/viewmodel/ChallengesViewModelTest.kt
+++ app/src/test/java/com/example/spin_bottle/viewmodel/ChallengesViewModelTest.kt
@@ -1,12 +1,19 @@
 package com.example.spin_bottle.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.spin_bottle.model.Challenge
 import com.example.spin_bottle.repository.ChallengeRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.setMain
 import org.junit.Assert.*
 
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
 
 class ChallengesViewModelTest {
 
@@ -20,5 +27,20 @@ class ChallengesViewModelTest {
     fun setUp() {
         challengesRepository = mock(ChallengeRepository::class.java)
         challengesViewModel = ChallengesViewModel(challengesRepository)
+    }
+
+    @Test
+    fun `test getChallengesList`() = runBlocking {
+        //Given
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+
+        val mockChallenges = mutableListOf(Challenge("1", "Test Challenge"))
+        `when`(challengesRepository.getChallengesList()).thenReturn(Result.success(mockChallenges))
+
+        //When
+        challengesViewModel.getChallengesList()
+
+        //Then
+        assertEquals(challengesViewModel.challengesList.value, mockChallenges)
     }
 }
--- app/src/test/java/com/example/spin_bottle/viewmodel/PokemonsViewModelTest.kt
+++ app/src/test/java/com/example/spin_bottle/viewmodel/PokemonsViewModelTest.kt
@@ -1,12 +1,20 @@
 package com.example.spin_bottle.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.spin_bottle.model.Challenge
+import com.example.spin_bottle.model.Pokemon
 import com.example.spin_bottle.repository.PokemonRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.setMain
 import org.junit.Assert.*
 
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
 
 class PokemonsViewModelTest {
 
@@ -20,5 +28,20 @@ class PokemonsViewModelTest {
     fun setUp() {
         pokemonRepository = mock(PokemonRepository::class.java)
         pokemonsViewModel = PokemonsViewModel(pokemonRepository)
+    }
+
+    @Test
+    fun `test getPokemons`() = runBlocking {
+        //Given
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+
+        val mockPokemons = mutableListOf(Pokemon(1, "https://Pokemon1"))
+        `when`(pokemonRepository.getPokemons()).thenReturn(mockPokemons)
+
+        //When
+        pokemonsViewModel.getPokemons()
+
+        //Then
+        assertEquals(pokemonsViewModel.pokemonsList.value, mockPokemons)
     }
 }
