d43a271b24368b7a6dde37519c718807ae57ba66
John <john.freddy.belalcazar@correounivalle.edu.co>
2024-11-28 16:33:15 -0500

    audio test +50

--- app/src/test/java/com/example/spin_bottle/viewmodel/AudioViewModelTest.kt
+++ app/src/test/java/com/example/spin_bottle/viewmodel/AudioViewModelTest.kt
@@ -1,10 +1,24 @@
 package com.example.spin_bottle.viewmodel
 
+import android.content.Context
+import android.media.MediaPlayer
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.spin_bottle_app.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Assert.*
 
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import org.mockito.MockedStatic
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.mockStatic
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
 
 class AudioViewModelTest {
 
@@ -12,9 +26,47 @@ class AudioViewModelTest {
     val rule = InstantTaskExecutorRule()
 
     private lateinit var audioViewModel: AudioViewModel
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var mediaPlayerStatic: MockedStatic<MediaPlayer>
+    private lateinit var context: Context
 
     @Before
     fun setUp() {
+        mediaPlayer = mock(MediaPlayer::class.java)
+        mediaPlayerStatic = mockStatic(MediaPlayer::class.java)
+        context = mock(Context::class.java)
         audioViewModel = AudioViewModel()
+    }
+
+    @After
+    fun tearDown() {
+        mediaPlayerStatic.close()
+    }
+
+    @Test
+    fun `test initMediaPlayer`() {
+        //Given
+        `when`(MediaPlayer.create(context, R.raw.background_music)).thenReturn(mediaPlayer)
+
+        //When
+        audioViewModel.initMediaPlayer(context)
+
+        //Then
+        assertNotNull(audioViewModel.mediaPlayer)
+    }
+
+    @Test
+    fun `test toggleAudio`() = runBlocking {
+        //Given
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+
+        val initAudioState = audioViewModel.isAudioOn.value
+        val expectedAudioState = !(audioViewModel.isAudioOn.value ?: true)
+
+        //When
+        audioViewModel.toggleAudio()
+
+        //Then
+        assertEquals(audioViewModel.isAudioOn.value, expectedAudioState)
     }
 }
