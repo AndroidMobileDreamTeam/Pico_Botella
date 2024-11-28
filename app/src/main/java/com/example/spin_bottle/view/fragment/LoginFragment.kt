8e22abce99a47ab407a4a582e390d7a3570fcdc3
Samuel Galindo <itzsaky@outlook.com>
2024-11-28 17:41:08 -0500

    fixed .kt import errors

--- app/src/main/java/com/example/spin_bottle/view/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/spin_bottle/view/fragment/LoginFragment.kt
@@ -1,6 +1,5 @@
 package com.example.spin_bottle.view.fragment
 
-import android.graphics.Typeface
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -8,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.compose.ui.text.font.Typeface
 import androidx.fragment.app.Fragment
 import com.example.spin_bottle_app.databinding.LoginFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
